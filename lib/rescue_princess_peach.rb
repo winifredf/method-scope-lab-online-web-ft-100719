8a8f2683acc24cc6c837ff21c799d86317e03d94
Winifred Frierson <winifred.dykes@hotmail.com>
2019-10-20 20:11:09 +0000

    Automatically backed up by Learn

--- lib/rescue_princess_peach.rb
+++ lib/rescue_princess_peach.rb
@@ -1,6 +1,10 @@
+require'pry'
+
 def rescue_princess_peach
   status = "rescued"
   puts "Hooray! Mario has rescued Princess Peach."
+  binding.pry
+  
 end
 
 ## Comment the below back in to see method scope in action!
